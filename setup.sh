5abf5b5bece04efb40b2bf71f2edd532c795541b
Pieter Delobelle <pieter.delobelle@gmail.com>
2023-10-22 13:24:47 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,16 @@
 
 echo installing node stuff
 
-NODE_MAJOR=16
+export NODE_MAJOR=16
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
 echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt update
 sudo apt install nodejs
+sudo apt install npm
 npm install
 
 echo installing python stuff
