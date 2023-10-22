c4f9d91ad9d13bc0a80f5ab330361c71f012f798
Pieter Delobelle <pieter.delobelle@gmail.com>
2023-10-22 13:36:00 +0200

    Updated setup.sh
    Fixed path to keyring

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ export NODE_MAJOR=16
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt update
